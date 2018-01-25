7c6a8b9548b4b3c868b8c84d2ea903122920fe3b
G003111118 <35809339+G003111118@users.noreply.github.com>
2018-01-25 16:59:01 +0000

    Add files via upload

---
+++ js/add-content.js
@@ -0,0 +1,28 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+var day = today.getDay();
+var weekDay;
+
+if(day == 3){
+		weekDay = "Wednesday";
+} else if (day ==4){
+		weekDay = "Thursday";
+} else if (day ==5){
+		weekDay = "Friday";
+}
+
+if (hourNow > 18) {
+	greeting = 'Good evening, its ' + weekDay;
+} else if (hourNow > 12) {
+	greeting = 'Good afternoon Stephen! Happy ' + weekDay;
+} else if (hourNow > 0) {
+	greeting = 'Good morning Stephen! Happy ' + weekDay;
+}else {
+	greeting = 'Welcome! ';
+}
+
+document.write('<h3>' + greeting + '</h3>'); 
+
+
